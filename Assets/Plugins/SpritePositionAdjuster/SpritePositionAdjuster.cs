a0cc0a1f2a111faa2f0259394122fa4d80b24511
Kili <kilimanjaro.a2@gmail.com>
2019-11-24 17:23:29 +0900

    Refactored SpritePositionAdjuster.cs

--- Assets/Plugins/SpritePositionAdjuster/SpritePositionAdjuster.cs
+++ Assets/Plugins/SpritePositionAdjuster/SpritePositionAdjuster.cs
@@ -6,33 +6,33 @@ namespace KiliWare
     {
         public float OriginalScreenHeight = 0f;
         public bool KeepsPositionUpdated = true;
-        protected Vector2 startPos;
-        protected Vector2 startLocalScale;
-        protected int beforeScreenWidth;
-        protected int beforeScreenHeight;
+        protected Vector2 startPosition;
+        protected Vector2 startScale;
+        protected int previousScreenWidth;
+        protected int previousScreenHeight;
 
-        void Awake()
+        protected void Awake()
         {
-            startLocalScale = transform.localScale;
-            startPos = transform.position - Camera.main.transform.position;
+            startScale = transform.localScale;
+            startPosition = transform.position - Camera.main.transform.position;
 
             #if UNITY_EDITOR
                 Debug.Log("Current screen height is: " + Screen.height + ". If you want to preserve current scale and position of this sprites, set OriginalScreenHeight property " + Screen.height + " and restart the scene to check whether it works correctly.");
             #endif
 
-            beforeScreenWidth = Screen.width;
-            beforeScreenHeight = Screen.height;
+            previousScreenWidth = Screen.width;
+            previousScreenHeight = Screen.height;
 
             adjustSpritePosition();
         }
-        
-        protected void adjustSpritePosition()
+
+        public void adjustSpritePosition()
         {
             float ajustedHeight = OriginalScreenHeight / Screen.height;
 
-            transform.localScale = new Vector2(startLocalScale.x, startLocalScale.y) * ajustedHeight;
+            transform.localScale = new Vector2(startScale.x, startScale.y) * ajustedHeight;
 
-            Vector2 adjustedPosition = startPos * ajustedHeight;
+            Vector2 adjustedPosition = startPosition * ajustedHeight;
             adjustedPosition.y += Camera.main.transform.position.y;
             transform.position = adjustedPosition;
         }
@@ -44,14 +44,14 @@ namespace KiliWare
             if (KeepsPositionUpdated && isScreenSizeChanged())
             {
                 adjustSpritePosition();
-                beforeScreenWidth = Screen.width;
-                beforeScreenHeight = Screen.height;
+                previousScreenWidth = Screen.width;
+                previousScreenHeight = Screen.height;
             }
         }
 
         protected bool isScreenSizeChanged()
         {
-            return beforeScreenHeight != Screen.height || beforeScreenWidth != Screen.width;
+            return previousScreenHeight != Screen.height || previousScreenWidth != Screen.width;
         }
     }
 }
